78e3c3c8ced0a1aaa423fca890f10a15631f263e
river portatil <riverflorez.04@gmail.com>
2024-12-25 12:16:10 -0500

    asda

--- src/app/api/students/[id]/grades/route.ts
+++ src/app/api/students/[id]/grades/route.ts
@@ -4,11 +4,12 @@ import clientPromise from '@/lib/mongodb'
 
 export async function POST(request: NextRequest, context: { params: { id: string } }) {
   try {
+    const { id } = await context.params // Esperar que `params` esté disponible
     const client = await clientPromise
     const db = client.db('studentstrack')
     const { grade } = await request.json()
 
-    const studentId = new ObjectId(context.params.id) // Usar context.params para obtener el ID
+    const studentId = new ObjectId(id) // Ahora `id` está correctamente resuelto
 
     // Asegúrate de que el campo grades sea un arreglo antes de intentar agregar la calificación
     const student = await db.collection("students").findOne({ _id: studentId })
